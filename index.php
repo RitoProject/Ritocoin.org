dd9fdc8c5b1c500f43b27d6e03a34e2849707b84
Jaspr <35606785+jaspr300@users.noreply.github.com>
2019-05-21 11:54:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -170,11 +170,15 @@ The developer fund is a key component of fostering a development-oriented commun
         </div>
         <div class="col-md-4 col-sm-4 cool_circle">
           <div class="cool_circle-ball">
+            <a href="https//wiki.ritocoin.org/wiki/Main_page/"><span class="glyphicon glyphicon-link"></span></a>
+          </div>
+          <h3><a href="https//wiki.ritocoin.org/wiki/Main_page/">Rito Wiki</a></h3>
+        </div> 
+    <div class="col-md-4 col-sm-4 cool_circle">
+          <div class="cool_circle-ball">
             <a href="https://ritocoin.asset-explorer.net/"><span class="glyphicon glyphicon-link"></span></a>
           </div>
-          <h3><a href="https://ritocoin.asset-explorer.net/">Asset Explorer</a></h3>
-        </div> 
-
+          <h3><a href="https://ritocoin.asset-explorer.net/">Asset Explorer</a></h3
       </div> 
     </div> 
 
