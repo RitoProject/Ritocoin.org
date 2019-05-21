35293d36224cb9afbd3b301b6fa5d8a91d68d81e
Jaspr <35606785+jaspr300@users.noreply.github.com>
2019-05-21 10:50:00 -0400

    Update index.php
    Added rito wiki

--- index.php
+++ index.php
@@ -211,7 +211,7 @@ The developer fund is a key component of fostering a development-oriented commun
 <div> 
 <div class="container">
       <h3 id="rito wiki" class="subhead">rito wiki</h3>
-      <center><a href="https://wiki.ritocoin.org/wiki/Main_Page” style="font-size:2em;">Rito Wiki</a></center>
+      <center><a href="https://wiki.ritocoin.org/wiki/Main_Page” style="font-size:2em;font-weight:bold;">Rito Wiki</a></center>
       <P>
     </div> <!-- /.container -->
 </div>
